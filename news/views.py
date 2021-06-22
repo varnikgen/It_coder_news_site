cef91ce7815ed66613308e13b3bbf09a32172830
Nikolay <varnikgen@yandex.ru>
2021-06-22 15:20:18 +1000

    IT Coder - Пользовательские теги шаблона

--- news/views.py
+++ news/views.py
@@ -6,18 +6,15 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news, 
         'title': 'Список новостей',
-        'categories': categories,
         }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, pk):
     news = News.objects.filter(category_id=pk)
-    categories = Category.objects.all()
     category = Category.objects.get(pk = pk)
-    context = {'news':news, 'categories': categories, 'category':category}
+    context = {'news':news, 'category':category}
     return render(request, template_name='news/category.html', context=context)
