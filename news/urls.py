99b13da5cd5a71249de33097a865881d99264c8d
Nikolay <varnikgen@yandex.ru>
2021-07-05 16:38:49 +1000

    IT Coder - Класс ListView p1

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),
+    path('', HomeNews.as_view(), name='home'),
     path('category/<int:pk>/', get_category, name='category'),
     path('news/<int:pk>/', view_news, name='view_news'),
     path('news/add-news/', add_news, name='add_news'),
